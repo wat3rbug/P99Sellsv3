26b031d42536db0ef0ce7ba754a2ad217812fe2c
Douglas Gardiner <doug.gardiner@gmail.com>
2021-01-06 10:57:35 -0600

    Basic setup of application.

---
+++ src/p99sellsv3/BuyerList.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p99sellsv3;
+
+/**
+ *
+ * @author douglas
+ */
+public class BuyerList {
+    
+}
